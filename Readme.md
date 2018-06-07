4daf1378ad8eebe0ea245f5d9970f9a9fad58368
SilviaDiniz <sreginardiniz@hotmail.com>
2018-06-07 13:29:25 -0300

    Meu commit do Produto Final -  isValidCard

---
+++ Readme.md
@@ -0,0 +1,21 @@
+README
+-> Usuário digita um número de cartão por meio de um prompt();
+
+-> Transforma uma string em array usando (array.push()), após será convertido para inteiro usando (paresInt());
+
+-> Inverter as posições o array com (reverse());
+
+-> Não será aceito campo vazio;
+
+-> Criou-se a function isValidCard();
+
+-> Criou-se variáveis para a manipulação dos valores;
+
+-> Foi feito um loop e uma consição para pegar somente os valores dos índices impares e multiplica-los por dois (array[i] * 2);
+
+-> Se o resultado da multiplicação for maior que 9 , subtrai-se 9 e soma-se com os demais valores dos índices pares que permaneceram inalterador;
+
+-> Para verificar se o cartão é válido, pega-se o resultado da soma total dos valores e aplica-se: (% 10 == 0);
+
+-> Caso o valor seja diferente( % 10! == 0 ), o cartão será inválido.
+# Produto-Final-isValidCard
---
+++ js/app.js
@@ -0,0 +1,38 @@
+var creditCard;
+var nCard;
+do {
+  creditCard = prompt('Digite o numero do cartão de crédito');
+  nCard = [];
+  for (var i = 0; i < creditCard.length; i++) {
+    nCard.push( parseInt(creditCard[i]) );
+  }
+  nCard.reverse();
+}
+while (nCard.length < 10);
+function isValidCard(creditCard) {
+  var total = 0,
+      result = 0,
+      subTotal = 0;
+  var position = 1;
+  for (var x = 0; x < nCard.length; x++) {
+    if (position == 2) {
+      subTotal = nCard[x] * 2;
+      if (subTotal > 9) {
+        subTotal = subTotal - 9;
+      }
+      total = total + subTotal;
+      position = 1;
+    } else {
+      position ++;
+      total = total + nCard[x];
+      result = total % 10;
+    }
+  }
+  if (result == 0) {
+      document.getElementById('valido').innerHTML = ('O cartão de número ' + creditCard + ' é valido');
+  } else {
+      document.getElementById('invalido').innerHTML = ('O cartão de número ' + creditCard + ' é invalido');
+  }
+ return result;
+}
+isValidCard(creditCard);
